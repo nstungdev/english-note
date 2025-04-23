ad2fec1ae121a3225898bf5292a85cb3e4317bba
nstungdev <nstung.dev@gmail.com>
2025-04-23 20:34:33 +0700

    Add TestController with a simple GET endpoint returning a hello world message

---
+++ api/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "hello world" });
+        }
+    }
+}
