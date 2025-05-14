7fe17ee1ab38f4e20bcecfc0029774bcb1cdc09a
Nguyen-Son-Tung <tung.sonnguyen@infodation.vn>
2025-05-14 11:12:44 +0700

    Update budget limits in angular.json; modify production and development API URLs in environment files

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5092/api', // Replace with your API URL
+  apiUrl: 'http://localhost:8080/api', // Replace with your API URL
 };
