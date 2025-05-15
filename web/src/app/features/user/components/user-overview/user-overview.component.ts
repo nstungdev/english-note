83c3b80b28df29d42d266a39fb5b8bcabb40c242
Nguyen-Son-Tung <tung.sonnguyen@infodation.vn>
2025-05-15 17:36:09 +0700

    Update Angular CLI and related packages to version 19.2.11; add user overview component with HTML and SCSS files.

---
+++ web/src/app/features/user/components/user-overview/user-overview.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-overview',
+  imports: [],
+  templateUrl: './user-overview.component.html',
+  styleUrl: './user-overview.component.scss',
+})
+export class UserOverviewComponent {}
